f17d258796e5c407b5a03992f352641777bc6d01
mengli <1054247914@qq.com>
2019-05-10 10:04:46 +0800

    uejdjd

--- renren-security/renren-admin/src/main/resources/statics/js/modules/account/workeraccountinfo.js
+++ renren-security/renren-admin/src/main/resources/statics/js/modules/account/workeraccountinfo.js
@@ -40,9 +40,14 @@ $(function () {
 var vm = new Vue({
 	el:'#rrapp',
 	data:{
+        queries:{
+            workerSfzh:null,
+            workerAccountNo:null
+        },
 		showList: true,
 		title: null,
-		workerAccountInfo: {}
+		workerAccountInfo: {},
+        bankDicJson:[]
 	},
 	methods: {
 		query: function () {
@@ -52,6 +57,8 @@ var vm = new Vue({
 			vm.showList = false;
 			vm.title = "新增";
 			vm.workerAccountInfo = {};
+            this.loadDic();
+            this.init();
 		},
 		update: function (event) {
 			var workerId = getSelectedRow();
@@ -64,27 +71,35 @@ var vm = new Vue({
             vm.getInfo(workerId)
 		},
 		saveOrUpdate: function (event) {
-		    $('#btnSaveOrUpdate').button('loading').delay(1000).queue(function() {
-                var url = vm.workerAccountInfo.workerId == null ? "sys/workeraccountinfo/save" : "sys/workeraccountinfo/update";
-                $.ajax({
-                    type: "POST",
-                    url: baseURL + url,
-                    contentType: "application/json",
-                    data: JSON.stringify(vm.workerAccountInfo),
-                    success: function(r){
-                        if(r.code === 0){
-                             layer.msg("操作成功", {icon: 1});
-                             vm.reload();
-                             $('#btnSaveOrUpdate').button('reset');
-                             $('#btnSaveOrUpdate').dequeue();
-                        }else{
-                            layer.alert(r.msg);
-                            $('#btnSaveOrUpdate').button('reset');
-                            $('#btnSaveOrUpdate').dequeue();
+            var url = vm.workerAccountInfo.workerId == null ? "sys/workeraccountinfo/save" : "sys/workeraccountinfo/update";
+            var bootstrapValidator = $("#projectForm").data('bootstrapValidator');
+            bootstrapValidator.validate();
+            if(bootstrapValidator.isValid()) {
+                $('#btnSaveOrUpdate').button('loading').delay(1000).queue(function() {
+                    $.ajax({
+                        type: "POST",
+                        url: baseURL + url,
+                        contentType: "application/json",
+                        data: JSON.stringify(vm.workerAccountInfo),
+                        success: function(r){
+                            if(r.code === 0){
+                                layer.msg("操作成功", {icon: 1});
+                                vm.reload();
+                                $('#btnSaveOrUpdate').button('reset');
+                                $('#btnSaveOrUpdate').dequeue();
+                            }else{
+                                layer.alert(r.msg);
+                                $('#btnSaveOrUpdate').button('reset');
+                                $('#btnSaveOrUpdate').dequeue();
+                            }
                         }
-                    }
+                    });
                 });
-			});
+
+            }else{
+                $('#btnSaveOrUpdate').button('reset');
+                $('#btnSaveOrUpdate').dequeue();
+            }
 		},
 		del: function (event) {
 			var workerIds = getSelectedRows();
@@ -123,9 +138,79 @@ var vm = new Vue({
 		reload: function (event) {
 			vm.showList = true;
 			var page = $("#jqGrid").jqGrid('getGridParam','page');
-			$("#jqGrid").jqGrid('setGridParam',{ 
+			$("#jqGrid").jqGrid('setGridParam',{
+                postData:{
+                    'workerSfzh': vm.queries.workerSfzh,
+                    'workerAccountNo': vm.queries.workerAccountNo
+                },
                 page:page
             }).trigger("reloadGrid");
-		}
-	}
+		},
+        loadDic:function(){
+            $.ajax({
+                url: baseURL + 'sys/dict/alllist',
+                type:'post',
+                data:{
+                    name:'银行编码'
+                },
+                success:function(data){
+                    if(data.msg=='success'){
+                        vm.bankDicJson=data.list;
+                    }else{
+                        layer.msg(data.msg);
+                    }
+                }
+            })
+        },
+        changeId:function(){
+            var text=$("#bankDicId").find('option:selected').text();
+            var code=$("#bankDicId").find('option:selected').attr("code");
+            vm.workerAccountInfo.bankCode=code;
+            vm.workerAccountInfo.bankName=text;
+        },
+        init:function(){
+            $('#projectForm').bootstrapValidator({
+                message: '输入信息有误',
+                feedbackIcons: {
+                    valid: 'glyphicon glyphicon-ok',
+                    invalid: 'glyphicon glyphicon-remove',
+                    validating: 'glyphicon glyphicon-refresh'
+                },
+                fields: {
+                    workerSfzh: {
+                        message: '输入内容有误',
+                        validators: {
+                            notEmpty: {
+                                message: '请输入工人身份证号'
+                            },
+                            regexp: {
+                                regexp:/(^\d{15}$)|(^\d{17}(\d|X|x)$)/,
+                                message: '身份证号格式错误'
+                            }
+                        }
+                    },
+                    bankDicId:{
+                        validators: {
+                            notEmpty: {
+                                message: '请选择开户行数据字典'
+                            }
+                        }
+                    },
+                    workerAccountNo:{
+                        validators: {
+                            notEmpty: {
+                                message: '请输入工人开户账号'
+                            },
+                            regexp: {
+                                regexp:/^(0[1-9][0-9]*)$/ ,
+                                message: '账户号只能输入数字'
+                            }
+                        }
+                    }
+                }
+            });
+            $('#projectForm').data('bootstrapValidator').resetForm(true);
+        }
+
+    }
 });
