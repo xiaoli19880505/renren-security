2933b31fdfb6ec0b03152d238e7988ea33702470
wangli <wangli_hero@163.com>
2019-05-15 11:08:27 +0800

    111

--- renren-security/renren-admin/src/main/java/io/renren/modules/account/service/impl/WorkerAccountInfoServiceImpl.java
+++ renren-security/renren-admin/src/main/java/io/renren/modules/account/service/impl/WorkerAccountInfoServiceImpl.java
@@ -5,6 +5,7 @@ import io.renren.common.utils.Constant;
 import io.renren.modules.account.dao.WorkerAccountInfoDao;
 import io.renren.modules.account.entity.WorkerAccountInfoEntity;
 import io.renren.modules.account.service.WorkerAccountInfoService;
+import org.apache.commons.lang.StringUtils;
 import org.springframework.stereotype.Service;
 import java.util.Map;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
@@ -21,9 +22,15 @@ public class WorkerAccountInfoServiceImpl extends ServiceImpl<WorkerAccountInfoD
     @Override
     @DataFilter(subDept = true, user = false,createDept = true)
     public PageUtils queryPage(Map<String, Object> params) {
+
+        String worker_sfzh = (String)params.get("workerSfzh");//工人身份证号
+        String worker_accountNo = (String)params.get("workerAccountNo");//工人账户号
+
         IPage<WorkerAccountInfoEntity> page = this.page(
                 new Query<WorkerAccountInfoEntity>().getPage(params),
                 new QueryWrapper<WorkerAccountInfoEntity>()
+                        .like(StringUtils.isNotBlank(worker_sfzh),"worker_sfzh",worker_sfzh)
+                        .like(StringUtils.isNotBlank(worker_accountNo),"worker_account_no",worker_accountNo)
                         .apply(params.get(Constant.SQL_FILTER) != null, (String)params.get(Constant.SQL_FILTER))
         );
 
